edfbdd5446f0582e5771a732d43c5dfb15ffea96
xYairrx <xyaireag@gmail.com>
2022-04-29 14:16:25 -0500

    Quinto programa

---
+++ src/program05/cuentalibro/nombre.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program05.cuentalibro;
+
+/**
+ *
+ * @author Yair
+ */
+public class nombre {
+    private String nombre;
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public void establecerNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    
+}
